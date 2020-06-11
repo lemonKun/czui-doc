a9fcd3d2f1f751f4a7dcdc0d8a55a28c10c899a2
lemonKun <798935263@qq.com>
2020-06-11 13:09:33 +0800

    build报错解决

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,7 +5,6 @@ import 'element-ui/lib/theme-chalk/index.css'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/googlecode.css' //样式文件
 
-import lkUi from 'lk-ui' // 要编写对应的文档的包
 import 'lk-ui/dist/lk-ui.css'
 Vue.directive('highlight', function (el) {
     let blocks = el.querySelectorAll('pre code');
@@ -20,5 +19,12 @@ export default ({
     siteData
 }) => {
     Vue.use(Element);
-    Vue.use(lkUi)
+    Vue.mixin({
+        mounted() {
+            //要编写对应的文档的包要通过这种mixin的方式引入，不然build的时候会报window is not defined的错
+            import('lk-ui').then(function (m) {
+                Vue.use(m.default)
+            })
+        },
+    })
 }
